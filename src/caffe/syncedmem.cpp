0d05456e8a0bcf3932cfd7a428f08806041b57ca
hanghliu <hangliu@cn.ibm.com>
2017-12-01 01:17:15 -0500

    more cleanup to the UVM optimizations

--- src/caffe/syncedmem.cpp
+++ src/caffe/syncedmem.cpp
@@ -6,14 +6,17 @@ namespace caffe {
 
 SyncedMemory::~SyncedMemory() {
   if (cpu_ptr_ && own_cpu_data_) {
-    CaffeFreeHost(cpu_ptr_, cpu_malloc_use_cuda_);
+    CaffeFreeHostOrDevice(cpu_ptr_, cpu_malloc_use_cuda_);
   }
 
 #ifndef CPU_ONLY
   if (gpu_ptr_ && own_gpu_data_) {
     int initial_device;
     cudaGetDevice(&initial_device);
-    cudaFree(gpu_ptr_);
+    if (gpu_device_ != -1) {
+       CUDA_CHECK_IGNORE_CUDART_UNLOADING(cudaSetDevice(gpu_device_));
+    }
+    CUDA_CHECK_IGNORE_CUDART_UNLOADING(cudaFree(gpu_ptr_));
     cudaSetDevice(initial_device);
   }
 #endif  // CPU_ONLY
@@ -22,7 +25,7 @@ SyncedMemory::~SyncedMemory() {
 inline void SyncedMemory::to_cpu() {
   switch (head_) {
   case UNINITIALIZED:
-    CaffeMallocHost(&cpu_ptr_, size_, &cpu_malloc_use_cuda_);
+    CaffeMallocManaged(&cpu_ptr_, size_, &cpu_malloc_use_cuda_);
     caffe_memset(size_, 0, cpu_ptr_);
     head_ = HEAD_AT_CPU;
     own_cpu_data_ = true;
@@ -31,7 +34,7 @@ inline void SyncedMemory::to_cpu() {
   case HEAD_AT_GPU:
 #ifndef CPU_ONLY
     if (gpu_ptr_ == NULL) {
-      CaffeMallocHost(&gpu_ptr_, size_, &cpu_malloc_use_cuda_);
+      CaffeMallocManaged(&gpu_ptr_, size_, &cpu_malloc_use_cuda_);
       own_cpu_data_ = true;
     }
     cpu_ptr_ = gpu_ptr_;
@@ -58,7 +61,6 @@ inline void SyncedMemory::to_gpu() {
     own_gpu_data_ = true;
     break;
   case HEAD_AT_CPU:
-    
     if (cpu_ptr_ == NULL) {
       CUDA_CHECK(cudaGetDevice(&gpu_device_));
       CUDA_CHECK(cudaMallocManaged(&cpu_ptr_, size_));
@@ -84,7 +86,7 @@ const void* SyncedMemory::cpu_data() {
 void SyncedMemory::set_cpu_data(void* data) {
   CHECK(data);
   if (own_cpu_data_) {
-    CaffeFreeHost(cpu_ptr_, cpu_malloc_use_cuda_);
+    CaffeFreeHostOrDevice(cpu_ptr_, cpu_malloc_use_cuda_);
   }
   cpu_ptr_ = data;
   head_ = HEAD_AT_CPU;
